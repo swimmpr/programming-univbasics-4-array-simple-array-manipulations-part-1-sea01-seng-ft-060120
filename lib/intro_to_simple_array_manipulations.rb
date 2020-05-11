3680a6c057c7bfdcd0a27bffc258b13b093bb463
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-11 21:46:35 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+ using_pop = array.pop()
+end
+
+def pop_with_args(array)
+  pop_with_args = array.pop(2)
+end
+
+def using_shift(array)
+  using_shift = array.shift()
+end
+
+def shift_with_args(array)
+  shift_with_args = array.shift(2)
+end
